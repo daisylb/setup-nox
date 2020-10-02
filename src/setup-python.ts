56e45cc55ab6c15e35a180bc64cdce5d47c989d2
Leigh Brenecki <l@leigh.net.au>
2020-10-02 19:24:10 +0930

    ghci test

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -1,6 +1,6 @@
 import {findAllVersions, find as findVersion} from '@actions/tool-cache';
-import {addPath} from '@actions/core'
-import {execSync} from 'child_process'
+import {addPath} from '@actions/core';
+import {execSync} from 'child_process';
 
 const IS_WINDOWS = process.platform === 'win32';
 
@@ -12,17 +12,17 @@ console.log(allCPythonVersions, allPyPyVersions);
 for (const version in allCPythonVersions) {
   const root = findVersion('Python', version);
   console.log(root);
-  if (IS_WINDOWS){
-    addPath(`${root}\Scripts`)
+  if (IS_WINDOWS) {
+    addPath(`${root}\Scripts`);
   } else {
-    addPath(`${root}/bin`)
+    addPath(`${root}/bin`);
   }
 }
 
 for (const version in allPyPyVersions) {
   const root = findVersion('PyPy', version);
   console.log(root);
-  addPath(`${root}/bin`)
+  addPath(`${root}/bin`);
 }
 
-execSync('pip install --user nox')
+execSync('pip install --user nox');
