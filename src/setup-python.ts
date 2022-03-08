e165618e8d994be3facf206f45f2fb0fdb18bf97
Leigh Brenecki <l@leigh.net.au>
2022-03-08 16:49:50 +1100

    Update versions of things, also other fixes
    Closes #419
    Closes #413

--- src/setup-python.ts
+++ src/setup-python.ts
@@ -1,7 +1,7 @@
 import { findAllVersions, find as findVersion } from "@actions/tool-cache"
 import { addPath } from "@actions/core"
 import { execSync } from "child_process"
-import { symlinkSync, readdirSync, readdir } from "fs"
+import { symlinkSync, readdirSync, readdir, existsSync } from "fs"
 
 //function installPythonVersion(type: "Python" | "PyPy")
 
@@ -10,24 +10,29 @@ const IS_WINDOWS = process.platform === "win32"
 const allCPythonVersions = findAllVersions("Python")
 const allPyPyVersions = findAllVersions("PyPy")
 
-console.log(allCPythonVersions, allPyPyVersions)
+console.log("Available CPython versions:", allCPythonVersions)
+console.log("Available PyPy versions:", allPyPyVersions)
+
+function symlinkIfNotExistsSync(target: string, link: string) {
+  if (!existsSync(link)) symlinkSync(target, link)
+}
 
 for (const version of allPyPyVersions) {
   const root = findVersion("PyPy", version)
-  console.log(root)
   addPath(`${root}/bin`)
-  if (/2\./.exec(version)) {
-    symlinkSync(`${root}/bin/pypy`, `${root}/bin/pypy2`)
+  if (/^2\./.test(version)) {
+    let minorVersion = /^2\.\d+/.exec(version)![0]
+    symlinkIfNotExistsSync(`${root}/bin/pypy`, `${root}/bin/pypy2`)
+    symlinkIfNotExistsSync(
+      `${root}/bin/pypy`,
+      `${root}/bin/pypy${minorVersion}`,
+    )
   }
 }
 
 for (const version of allCPythonVersions) {
-  console.log(version)
   const root = findVersion("Python", version)
-  console.log(root)
-  console.log("root dir contents", readdirSync(root))
   if (IS_WINDOWS) {
-    console.log("scripts dir contents", readdirSync(`${root}\\Scripts`))
     addPath(root)
     addPath(`${root}\\Scripts`)
   } else {
@@ -36,6 +41,7 @@ for (const version of allCPythonVersions) {
 }
 
 const NOX_PYTHON_VERSION = allCPythonVersions[allCPythonVersions.length - 1]
+console.log("Nox itself will be installed using", NOX_PYTHON_VERSION)
 const NOX_PYTHON_PATH =
   findVersion("Python", NOX_PYTHON_VERSION) +
   (IS_WINDOWS ? "\\python.exe" : "/bin/python")
